d8cd562e4b8f0d456d9bf1fae3a10e135c21cd00
Terrance <terrancehah@gmail.com>
2025-06-27 11:06:53 +0800

    refactor: convert Document component from function to class-based component

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,17 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
 
-export default function Document() {
-  return (
-    <Html lang={this.props.__NEXT_DATA__.props.pageProps?.locale || 'en'}>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        {/* <script async src="https://js.stripe.com/v3/buy-button.js"></script> */}
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+export default class Document extends NextDocument {
+  render() {
+    return (
+      <Html lang={this.props.__NEXT_DATA__.props.pageProps?.locale || 'en'}>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+          {/* <script async src="https://js.stripe.com/v3/buy-button.js"></script> */}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
